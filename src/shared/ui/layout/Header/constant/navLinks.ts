c694ff717e6fdb2b2be1c709ffa1dbdca31ca8d0
Сергей Глотов <s.glotov@estelink.ru>
2024-10-06 17:32:27 +0300

    fix: routes

--- src/shared/ui/layout/Header/constant/navLinks.ts
+++ src/shared/ui/layout/Header/constant/navLinks.ts
@@ -1,6 +1,6 @@
 export const NAV_LINKS = [
     { name: 'О компании', link: '/about' },
-    { name: 'Проекты', link: '/projects' },
+    { name: 'Проекты', link: '/projects/main' },
     { name: 'Услуги', link: '/services' },
     { name: 'Новости', link: '/news' },
     { name: 'Блог', link: '/blog' },
