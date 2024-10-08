0f98116246c37d417c5a6a4a42bdd77f7616ef6c
Сергей Глотов <s.glotov@estelink.ru>
2024-10-08 19:29:07 +0300

    fix: dynamic import

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,109 +1,112 @@
-import {
-    createRouter,
-    createWebHistory,
-    RouteRecordRaw,
-} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [
+import MainPage from 'src/pages/Main/MainPage.vue'
+import AboutPage from 'src/pages/About/AboutPage.vue'
+import ContactsPage from 'src/pages/Contacts/ContactsPage.vue'
+import BlogMainPage from 'src/pages/Blog/BlogMainPage.vue'
+import BlogVue from 'src/pages/Blog/Blog.vue'
+import BlogTypePage from 'src/pages/Blog/BlogTypePage.vue'
+import FAQPageVue from 'src/pages/FAQ/FAQPage.vue'
+import ProjectVue from 'src/pages/Projects/Project.vue'
+import ProjectTypePageVue from 'src/pages/Projects/ProjectTypePage.vue'
+import ProjectMainPageVue from 'src/pages/Projects/ProjectMainPage.vue'
+import ServicesMainPageVue from 'src/pages/Services/ServicesMainPage.vue'
+import ServicesPageVue from 'src/pages/Services/ServicesPage.vue'
+import ServicesTypePageVue from 'src/pages/Services/ServicesTypePage.vue'
+import NewsVue from 'src/pages/News/News.vue'
+import NewsMainPageVue from 'src/pages/News/NewsMainPage.vue'
+import NewsTypePageVue from 'src/pages/News/NewsTypePage.vue'
+const routes = [
     {
         path: '/',
-        component: () => import('src/pages/Main/MainPage.vue'),
+        component: MainPage,
         name: 'home',
     },
 
     {
         path: '/about',
-        component: () => import('src/pages/About/AboutPage.vue'),
+        component: AboutPage,
         name: 'about',
     },
     {
         path: '/contacts',
-        component: () =>
-            import('src/pages/Contacts/ContactsPage.vue'),
+        component: ContactsPage,
         name: 'contacts',
     },
     {
         path: '/blog',
-        component: () => import('src/pages/Blog/Blog.vue'),
+        component: BlogVue,
         redirect: '/blog/main',
         name: 'blog',
         children: [
             {
                 path: ':id',
-                component: () =>
-                    import('src/pages/Blog/BlogTypePage.vue'),
+                component: BlogTypePage,
                 name: 'BlogTypePage',
             },
             {
                 path: 'main',
-                component: () =>
-                    import('src/pages/Blog/BlogMainPage.vue'),
+                component: BlogMainPage,
                 name: 'BlogMainPage',
             },
         ],
     },
     {
         path: '/questions',
-        component: () => import('src/pages/FAQ/FAQPage.vue'),
+        component: FAQPageVue,
         name: 'faq',
     },
     {
         path: '/projects',
-        component: () => import('src/pages/Projects/Project.vue'),
+        component: ProjectVue,
         name: 'projects',
         redirect: '/projects/main',
         children: [
             {
                 path: ':id',
-                component: () =>
-                    import('src/pages/Projects/ProjectTypePage.vue'),
+                component: ProjectTypePageVue,
                 name: 'ProjectTypePage',
             },
             {
                 path: 'main',
-                component: () =>
-                    import('src/pages/Projects/ProjectMainPage.vue'),
+                component: ProjectMainPageVue,
                 name: 'ProjectsMainPage',
             },
         ],
     },
     {
         path: '/services',
-        component: () =>
-            import('src/pages/Services/ServicesPage.vue'),
+        component: ServicesPageVue,
         name: 'services',
         redirect: '/services/main',
         children: [
             {
                 path: 'main',
-                component: () =>
-                    import('src/pages/Services/ServicesMainPage.vue'),
+                component: ServicesMainPageVue,
                 name: 'ServicesMainPage',
             },
             {
                 path: 'create',
-                component: () =>
-                    import('src/pages/Services/ServicesTypePage.vue'),
+                component: ServicesTypePageVue,
                 name: 'ServicesTypePage',
             },
         ],
     },
     {
         path: '/news',
-        component: () => import('src/pages/News/News.vue'),
+        component: NewsVue,
         redirect: '/news/main',
         name: 'news',
         children: [
             {
                 path: 'main',
-                component: () =>
-                    import('src/pages/News/NewsMainPage.vue'),
+                component: NewsMainPageVue,
+
                 name: 'NewsMainPage',
             },
             {
                 path: ':id',
-                component: () =>
-                    import('src/pages/News/NewsTypePage.vue'),
+                component: NewsTypePageVue,
                 name: 'NewsTypePage',
             },
         ],
